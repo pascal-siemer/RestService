dd8f80bef9c0b889c2a080748e156cb779792541
pascal-siemer <95932011+pascal-siemer@users.noreply.github.com>
2023-07-20 00:01:39 +0200

    Programm-Initialisierung gespaltet.

--- RestService/Program.cs
+++ RestService/Program.cs
@@ -1,18 +1,9 @@
-
-using RestService.Data;
-using RestService.DatabaseDriver;
-using RestService.DataMapper;
-using RestService.Endpoint;
-
-
-var connectionString = "Data Source=(local);Initial Catalog=Calendar;Integrated Security=SSPI";
+using RestService;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddSingleton<IDatabaseDriver>(new MSSQLDriver(connectionString));
-builder.Services.AddSingleton<IDataMapper<User>>(new UserMapper());
-builder.Services.AddSingleton<UserEndpoint>();
+Services.InitServices(builder);
 
 var app = builder.Build();
-app.MapGet("/", () => "Hello World!");
-app.MapGet("/users", (UserEndpoint endpoint) => endpoint.Get().ToString());
+Router.InitRoutes(app);
+
 app.Run();
---
+++ RestService/Services.cs
@@ -0,0 +1,18 @@
+﻿using RestService.Data;
+using RestService.DatabaseDriver;
+using RestService.DataMapper;
+using RestService.Endpoint;
+
+namespace RestService;
+
+public static class Services
+{
+    private static readonly string _connectionString = "Data Source=(local);Initial Catalog=Calendar;Integrated Security=SSPI";
+    
+    public static void InitServices(WebApplicationBuilder builder)
+    {
+        builder.Services.AddSingleton<IDatabaseDriver>(new MSSQLDriver(_connectionString));
+        builder.Services.AddSingleton<IDataMapper<User>>(new UserMapper());
+        builder.Services.AddSingleton<UserEndpoint>();
+    }
+}
